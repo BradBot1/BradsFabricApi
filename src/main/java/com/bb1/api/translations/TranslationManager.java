054989655d8f32dabad43939bdfbc558a497324f
BradBot1 <Bradley6733@outlook.com>
2021-06-11 14:06:26 +0100

    licensing

--- src/main/java/com/bb1/api/translations/TranslationManager.java
+++ src/main/java/com/bb1/api/translations/TranslationManager.java
@@ -28,6 +28,21 @@ import fr.catcore.server.translations.api.resource.language.ServerLanguageDefini
 import fr.catcore.server.translations.api.resource.language.TranslationMap;
 import fr.catcore.server.translations.api.resource.language.TranslationsReloadListener;
 
+/**
+ * Copyright 2021 BradBot_1
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public final class TranslationManager implements TranslationsReloadListener {
 	/**
 	 * The instance of {@link ServerTranslations} that we use to modify translations
